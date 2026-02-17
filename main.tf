b0bb215bc6b543dc6e98420c5d0ff0987bd3f55c
Jonah Rosenberg <jrosenberg272@gmail.com>
2026-02-16 23:27:20 -0500

    added remote state

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "perfect-car-picker-tf-state"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "perfect-car-picker-tf-locks"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
