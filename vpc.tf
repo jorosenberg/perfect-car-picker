767035742afa9b96acf35e863dabe300fe7cd713
Jonah Rosenberg <jrosenberg272@gmail.com>
2026-02-17 00:57:03 -0500

    attach public subnet to route table

--- vpc.tf
+++ vpc.tf
@@ -116,3 +116,8 @@ resource "aws_route_table" "public_rt" {
     Name = "${var.project_name}-public-rt"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
