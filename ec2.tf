a2dbb3f873d00848702579f5d0ae390597c75f24
Jonah Rosenberg <jrosenberg272@gmail.com>
2026-02-16 23:08:37 -0500

    update ec2 to free tier eligible, --provenance=false docker

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_server" {
   ami                  = "ami-0c7217cdde317cfec" 
-  instance_type        = "t2.micro"
+  instance_type        = "t3.micro"
   subnet_id            = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
