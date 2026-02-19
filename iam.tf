3c2cc10a20498d7d8caf30d8a174768148846cf5
Jonah Rosenberg <jrosenberg272@gmail.com>
2026-02-19 16:02:22 -0500

    clean iam

--- iam.tf
+++ iam.tf
@@ -30,9 +30,8 @@ resource "aws_iam_role" "iam_for_ec2" {
 EOF
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
+resource "aws_iam_policy" "lambda_policy" {
   name = "${var.project_name}-lambda-policy"
-  role = aws_iam_role.iam_for_lambda.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -59,39 +58,9 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
-resource "aws_iam_policy" "bedrock_access" {
-  name        = "${var.project_name}-bedrock-policy"
-  description = "Allow access to Bedrock models"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "bedrock:InvokeModel",
-        "bedrock:ListFoundationModels"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.lambda_policy.arn
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_bedrock" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.bedrock_access.arn
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_bedrock" {
-  role       = aws_iam_role.iam_for_ec2.name
-  policy_arn = aws_iam_policy.bedrock_access.arn
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
