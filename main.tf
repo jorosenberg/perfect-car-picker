db452247153bfd81da6926fe834b198ba8b42ea2
Jonah Rosenberg <49052921+jorosenberg@users.noreply.github.com>
2026-04-13 14:38:32 -0400

    update backend lock

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
     key = "terraform.tfstate"
     region = "us-east-1"
     encrypt = true
-    dynamodb_table = "perfect-car-picker-tf-locks"
+    dynamodb_table = "perfect-car-picker-tfstate"
   }
 }
 
